e452fc088d252f058cc0566f57a51360b8f4521a
PepperMintTelugu <peppermint.telugu@gmail.com>
2025-08-01 07:38:59 +0530

    deleted
    deleted

--- frontend/src/components/homepage/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all homepage components
-export { default as HeroSlider } from "./HeroSlider";
-export { default as CategoriesSection } from "./CategoriesSection";
-export { default as AuthorsCarousel } from "./AuthorsCarousel";
-export { default as PublishersCarousel } from "./PublishersCarousel";
-export { default as StatsSection } from "./StatsSection";
-export { default as WhyChooseUsSection } from "./WhyChooseUsSection";
-export { default as NewsletterSection } from "./NewsletterSection";
